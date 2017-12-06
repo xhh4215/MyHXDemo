852d21c6b07c653589f615e06b9410cd262a6af8
xhh <18734184215@163.com>
2017-12-06 17:09:34 +0800

    使用环信基本的实现登陆， 登出 ，聊天等功能

--- src/main/java/com/example/xhh/myhxdemo/MainActivity.java
+++ src/main/java/com/example/xhh/myhxdemo/MainActivity.java
@@ -1,13 +1,90 @@
 package com.example.xhh.myhxdemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.hyphenate.EMCallBack;
+import com.hyphenate.chat.EMClient;
 
 public class MainActivity extends AppCompatActivity {
+    // 发起聊天 username 输入框
+    private EditText mChatIdEdit;
+    // 发起聊天
+    private Button mStartChatBtn;
+    // 退出登录
+    private Button mSignOutBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (!EMClient.getInstance().isLoggedInBefore()) {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+            return;
+
+        }
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        mChatIdEdit = (EditText) findViewById(R.id.ec_edit_chat_id);
+
+        mStartChatBtn = (Button) findViewById(R.id.ec_btn_start_chat);
+        mStartChatBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String chatId = mChatIdEdit.getText().toString().trim();
+                if (!TextUtils.isEmpty(chatId)) {
+                    String currentname = EMClient.getInstance().getCurrentUser();
+                    if (chatId.equals(currentname)) {
+                        Toast.makeText(MainActivity.this, "不能和自己聊天", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    // 跳转到聊天界面，开始聊天
+                    Intent intent = new Intent(MainActivity.this, ChatActivity.class);
+                    intent.putExtra("ec_chat_id", chatId);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(MainActivity.this, "Username 不能为空", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        mSignOutBtn = (Button) findViewById(R.id.ec_btn_sign_out);
+        mSignOutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signOut();
+            }
+        });
+    }
+
+    private void signOut() {
+        EMClient.getInstance().logout(false, new EMCallBack() {
+            @Override
+            public void onSuccess() {
+                Log.i("lzan13", "logout success");
+                // 调用退出成功，结束app
+                finish();
+            }
+
+            @Override
+            public void onError(int i, String s) {
+                Log.i("lzan13", "logout error " + i + " - " + s);
+            }
+
+            @Override
+            public void onProgress(int i, String s) {
+
+            }
+        });
     }
 }
